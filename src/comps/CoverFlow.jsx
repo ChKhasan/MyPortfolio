088db339f1d9290ccf8fb48403637703c574450b
Khasan_Ch <chulliyevhasan1405@mail.ru>
2022-05-03 22:49:44 +0500

    cover flow change

--- src/comps/CoverFlow.jsx
+++ src/comps/CoverFlow.jsx
@@ -1,40 +1,102 @@
-import React from 'react';
-import Coverflow from 'react-coverflow';
+import React from "react";
+import Coverflow from "react-coverflow";
 
 const CoverFlow = () => {
-    const fn = function () {
-        /* do your action */
-      }
+  const fn = function () {
+    /* do your action */
+  };
   return (
-      <div className='container-xxl'>
+    <div className="container-fluid" style={{background: "rgba(0, 0, 0, 0.1)"}}>
 
-    <Coverflow width="960" height="500"
-    displayQuantityOfSide={2}
-    navigation={false}
-    enableScroll={true}
-    clickable={true}
-    active={0}
-  >
-    <div
-      onClick={() => fn()}
-      onKeyDown={() => fn()}
-      role="menuitem"
-      tabIndex="0"
-    >
-      <img
-        src='./Images/css.png'
-        alt='title or description'
-        style={{
-          display: 'block',
-          width: '100%',
-        }}
-      />
+    <div className="container-xxl">
+      <Coverflow
+        width="960"
+        height="500"
+        displayQuantityOfSide={2}
+        navigation={false}
+        enableScroll={true}
+        clickable={true}
+        active={0}
+      >
+        <div
+          onClick={() => fn()}
+          onKeyDown={() => fn()}
+          role="menuitem"
+          tabIndex="0"
+        >
+          <img
+            src="./Images/Screenshot from 2022-05-03 21-27-58.png"
+            alt="title or description"
+            data-action="https://ecstatic-aryabhata-30dd3d.netlify.app/"
+            style={{
+              display: "block",
+              width: "100%",
+            }}
+          />
+        </div>
+        <img
+          src="./Images/Screenshot from 2022-05-03 21-27-58.png"
+          alt="title or description"
+          data-action="https://ecstatic-aryabhata-30dd3d.netlify.app/"
+        />
+        <img
+          src="./Images/IMG_20211115_234554.jpg"
+          alt="title or description"
+        />
+        <img
+          src="./Images/Screenshot from 2022-05-03 20-22-13.png"
+          alt="title or description"
+          data-action="https://ecommerce-pizza.netlify.app/"
+        />
+        <img
+          src="./Images/Screenshot from 2022-05-03 21-38-24.png"
+          alt="title or description"
+          data-action="https://linked-in-36.netlify.app/"
+        />
+        <img
+          src="./Images/Screenshot from 2022-05-03 21-45-59.png"
+          alt="title or description"
+          data-action="https://ionic-first-change.netlify.app/"
+        />
+        <img
+          src="./Images/Screenshot from 2022-05-03 21-50-48.png"
+          alt="title or description"
+          data-action="https://product2-online.netlify.app/"
+        />
+        <img
+          src="./Images/Screenshot from 2022-05-03 21-54-48.png"
+          alt="title or description"
+          data-action="https://online-food-shopone.netlify.app/"
+        />
+        <img
+          src="./Images/Screenshot from 2022-05-03 22-00-09.png"
+          alt="title or description"
+          data-action="https://proect-safia-bakery-github.netlify.app/"
+        />
+        <img
+          src="./Images/Screenshot from 2022-05-03 22-05-35.png"
+          alt="title or description"
+          data-action="https://comfy-muffin-8a2a13.netlify.app/"
+        />
+        <img
+          src="./Images/Screenshot from 2022-05-03 22-08-31.png"
+          alt="title or description"
+          data-action="https://api-jsonplacholder.netlify.app/"
+        />
+        <img
+          src="./Images/Screenshot from 2022-05-03 22-15-29.png"
+          alt="title or description"
+          data-action="https://cardmon-c2.netlify.app/"
+        />
+        <img
+          src="./Images/Screenshot from 2022-05-03 22-18-34.png"
+          alt="title or description"
+          data-action="https://xenodochial-jones-29db7f.netlify.app/"
+        />
+      </Coverflow>
     </div>
-    <img src='./Images/IMG_20211115_234554.jpg' alt='title or description' />
-    <img src='./Images/10001.png' alt='title or description' />
-  </Coverflow>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default CoverFlow
+export default CoverFlow;
